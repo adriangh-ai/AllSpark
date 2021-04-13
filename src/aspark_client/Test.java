709532f5334bc80046d5756b5c3c872bd741fd14
adriangh-ai <aghajari1@alumno.uned.es>
2021-04-14 00:34:26 +0200

    Test VSCode

--- src/aspark_client/Test.java
+++ src/aspark_client/Test.java
@@ -1,6 +1,6 @@
 package aspark_client;
 class Test {
     public static void main(String[] args){
-        System.out.println("Mehehehehe");
+        System.out.println("Este cambio se ha guardado bien");
     }
 }
