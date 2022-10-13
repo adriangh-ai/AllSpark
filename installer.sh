040f60adb829e3c8bb798ed5d229def346b09ad4
adriangh-ai <aghajari1@alumno.uned.es>
2022-10-13 22:47:53 +0200

    updated installer.sh with torch 11.6

--- installer.sh
+++ installer.sh
@@ -2,7 +2,8 @@
 ##TEMPORAL##
 python3 -m venv $HOME/AllSpark_data/AllSpark_venv
 source $HOME/AllSpark_data/AllSpark_venv/bin/activate
-pip3 install -U wheel torch transformers numpy==1.20 plotly scikit-learn pandas psutil nltk datasets dash grpcio 
-pip3 install -U grpcio-tools waitress dash-uploader dash-daq umap-learn sentencepiece protobuf gensim protobuf3-to-dict
+pip3 install -U torch --extra-index-url https://download.pytorch.org/whl/cu116
+pip3 install -U wheel transformers numpy==1.20 plotly scikit-learn pandas psutil nltk datasets dash grpcio 
+pip3 install -U grpcio-tools==1.38 waitress dash-uploader dash-daq umap-learn sentencepiece protobuf==3.19 gensim protobuf3-to-dict
 python3 -m nltk.downloader punkt
 deactivate
